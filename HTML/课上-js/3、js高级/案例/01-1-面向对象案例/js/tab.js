db39e7de9706125806f96cce1a0f50b6e23a1e7c
XiaoE4 <414546312@qq.com>
2022-10-18 11:27:00 +0800

    master

--- HTML/课上-js/3、js高级/案例/01-1-面向对象案例/js/tab.js
+++ HTML/课上-js/3、js高级/案例/01-1-面向对象案例/js/tab.js
@@ -1,52 +1,104 @@
 
-var that=this;
+var that
 
-class Tab{
-    constructor(id){
-        that=this;
+class Tab {
+    constructor(id) {
+        that = this;
         // 获取元素
-        this.main=document.querySelector(id);
-        this.lis=this.main.querySelectorAll('li');
-        this.section=this.main.querySelectorAll('section')
-        this.add=this.main.querySelector('.tabadd')
-        this.ul=this.main.querySelector('.firstnav ul:first-child')
+        this.main = document.querySelector(id);
+        this.add = this.main.querySelector('.tabadd')
+        // li父元素
+        this.ul = this.main.querySelector('.fisrstnav ul:first-child')
+        // section父元素
+        this.fsection = this.main.querySelector('.tabscon')
         console.log(this.lis);
         this.init()
     }
-    init(){
-        this.add.onclick=this.addTab;
-        for(let i=0;i<this.lis.length;i++){
-            this.lis[i].index=i
-            this.lis[i].onclick=this.toggleTab;
+    init() {
+        this.updateNode()
+        this.add.onclick = this.addTab;
+        for (var i = 0; i < this.lis.length; i++) {
+            this.lis[i].index = i
+            this.lis[i].onclick = this.toggleTab;
+            this.remove[i].onclick = this.removeTab;
+            this.spans[i].ondblclick = this.editTab;
+            this.sections[i].ondblclick = this.editTab;
         }
+    }
+    // 重新获取元素
+    updateNode() {
+        this.lis = this.main.querySelectorAll('li')
+        this.sections = this.main.querySelectorAll('section')
+        this.remove = this.main.querySelectorAll('.icon-guanbi')
+        this.spans = this.main.querySelectorAll('.fisrstnav li span:first-child');
     }
     // 1、切换功能
     toggleTab() {
         // console.log(this.index);
         that.clearClass()
-        this.className='liactive'
-        that.section[this.index].className='conactive'
+        this.className = 'liactive'
+        that.sections[this.index].className = 'conactive'
         console.log(this);
         console.log(that);
     }
-    clearClass(){
-        for(let i=0;i<this.lis.length;i++){
-            this.lis[i].className=''
-            that.section[i].className=''
+    clearClass() {
+        for (var i = 0; i < this.lis.length; i++) {
+            this.lis[i].className = ''
+            that.sections[i].className = ''
         }
     }
     // 2、添加功能
     addTab() {
+        that.clearClass();
+        let random=Math.random()
         // 1、创建li和section
-        var li='<li class="liactive"><span>测试1</span><span class="iconfont icon-guanbi"></span></li>'
+        var li='<li class="liactive"><span>测试1</span><span class="iconfont icon-guanbi"></span></li>';
+        var section = '<section class="conactive">测试 ' + random + '</section>';
         // 2、添加到父元素
-        that.ul.insertAdjacentHTML('beforeend',li);
-
+        that.ul.insertAdjacentHTML('beforeend', li);
+        that.fsection.insertAdjacentHTML('beforeend', section);
+        that.init()
     }
     // 3、删除功能
-    removeTab() {}
+    removeTab(e) {
+        e.stopPropagation(); // 阻止冒泡 防止触发li 的切换点击事件
+        var index = this.parentNode.index;
+        console.log(index);
+        // 根据索引号删除对应的li 和section   remove()方法可以直接删除指定的元素
+        that.lis[index].remove();
+        that.sections[index].remove();
+        that.init();
+        // 当我们删除的不是选中状态的li 的时候,原来的选中状态li保持不变
+        if (document.querySelector('.liactive')) return;
+        // 当我们删除了选中状态的这个li 的时候, 让它的前一个li 处于选定状态
+        index--;
+        // 手动调用我们的点击事件  不需要鼠标触发
+        that.lis[index] && that.lis[index].click();
+
+    }
     // 4、修改功能
-    editTab() {}
+    editTab() {
+        var str = this.innerHTML;
+        // 双击禁止选定文字
+        window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
+        // alert(11);
+        this.innerHTML = '<input type="text" />';
+        var input = this.children[0];
+        input.value = str;
+        input.select(); // 文本框里面的文字处于选定状态
+        // 当我们离开文本框就把文本框里面的值给span 
+        input.onblur = function () {
+            this.parentNode.innerHTML = this.value;
+        };
+        // 按下回车也可以把文本框里面的值给span
+        input.onkeyup = function (e) {
+            if (e.keyCode === 13) {
+                // 手动调用表单失去焦点事件  不需要鼠标离开操作
+                this.blur();
+            }
+        }
+
+    }
 
 
 }
@@ -77,7 +129,6 @@ class Tab {
                 this.remove[i].onclick = this.removeTab;
                 this.spans[i].ondblclick = this.editTab;
                 this.sections[i].ondblclick = this.editTab;
-
             }
         }
         // 因为我们动态添加元素 需要从新获取对应的元素
