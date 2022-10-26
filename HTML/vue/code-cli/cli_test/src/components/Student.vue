6a618becbf706eb156c49b13654240ad8ed01f95
XiaoE4 <414546312@qq.com>
2022-10-26 11:34:19 +0800

    master

---
+++ HTML/vue/code-cli/cli_test/src/components/Student.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <h2>学生姓名：{{name}}</h2>
+    <h2>学生性别：{{sex}}</h2>
+    <h2>学生年龄：{{age}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'WorkspaceJsonSchool',
+    data() {
+        return {
+          msg:'我来自中国',
+          
+        };
+    },
+};
+</script>
+
+
+
